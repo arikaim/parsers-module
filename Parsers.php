3403203ac9b50234ed59cead0fae1fb071fe18b1
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 <sales@intersoft.biz>
2021-10-19 18:42:21 +0300

    =

---
+++ Parsers.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c) Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+*/
+namespace Arikaim\Modules\Parsers;
+
+use Arikaim\Core\Extension\Module;
+
+/**
+ * Parsers module class
+ */
+class Parsers extends Module
+{
+    /**
+     * Install module
+     *
+     * @return void
+     */
+    public function install()
+    {        
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## Arikaim CMS Parsers Module
+![1.0.0](https://img.shields.io/github/release/arikaim/parsers-module.svg)
+![GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)
+
+Html, css parsers
+
+#### Requirements  
+  * [Arikaim CMS](https://github.com/arikaim/arikaim)
+
+
+#### Installation
+
+```sh
+composer require arikaim/parsers-module
+```
